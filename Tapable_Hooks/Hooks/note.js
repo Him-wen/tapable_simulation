b2babb61eab1c79296ac05af061bcb0e67c3ef55
“him-wen” <wenvoi@163.com>
2022-03-20 16:21:09 +0800

    commit

--- Tapable_Hooks/Hooks/note.js
+++ Tapable_Hooks/Hooks/note.js
@@ -2,3 +2,19 @@
 do...while
 reduce
  */
+const {
+    SyncHook,
+    SyncBailHook,
+    SyncWaterfallHook,
+    SyncLoopHook,
+    AsyncParallelHook,
+    AsyncParallelBailHook,
+    AsyncSeriesHook,
+    AsyncSeriesBailHook,
+    AsyncSeriesWaterfallHook
+ } = require("tapable");
+
+ /**
+  * 这些钩子可分为同步的钩子和异步的钩子，Sync开头的都是同步的钩子，Async开头的都是异步的钩子。
+  * 而异步的钩子又可分为并行和串行，其实同步的钩子也可以理解为串行的钩子。
+  */
