b6300a52d9d23f490b2634ac294a39b6a6168c6e
“him-wen” <wenvoi@163.com>
2022-03-20 17:57:13 +0800

    ✅异步钩子

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # tapable_simulation
 Simple simulation of tapable hook function workflow
 手写Tapable钩子
+
+SyncHook,//✅同步
+SyncBailHook,//✅同步
+SyncWaterfallHook,//✅同步
+SyncLoopHook,
+AsyncParallelHook,//异步✅
+AsyncParallelBailHook,
+AsyncSeriesHook,//异步✅
+AsyncSeriesBailHook,
+AsyncSeriesWaterfallHook
--- Tapable_Hooks/Hooks/_synnote.js
+++ Tapable_Hooks/Hooks/_synnote.js
@@ -3,13 +3,13 @@ do...while
 reduce
  */
 const {
-    SyncHook,//同步
-    SyncBailHook,
-    SyncWaterfallHook,
+    SyncHook,//✅同步
+    SyncBailHook,//✅同步
+    SyncWaterfallHook,//✅同步
     SyncLoopHook,
-    AsyncParallelHook,//异步并行
+    AsyncParallelHook,//异步✅
     AsyncParallelBailHook,
-    AsyncSeriesHook,//异步串行
+    AsyncSeriesHook,//异步✅
     AsyncSeriesBailHook,
     AsyncSeriesWaterfallHook
  } = require("tapable");
@@ -17,4 +17,7 @@ const {
  /**
   * 这些钩子可分为同步的钩子和异步的钩子，Sync开头的都是同步的钩子，Async开头的都是异步的钩子。
   * 而异步的钩子又可分为并行和串行，其实同步的钩子也可以理解为串行的钩子。
+  * 
   */
+
+ 
